8e2d93fa2b0e51f4dc68c9386141e00866d41c70
Fernando Escobar <fernando666@gmail.com>
2017-08-17 20:35:03 -0400

    o_O

--- Arena.py
+++ Arena.py
@@ -21,29 +21,29 @@ class Run(Main):
                     self.battle.ability(0, -400, "provoke")
                     # self.battle.dual_ability(0, -400, "dualcast", 200, "curaja", 0)
                 except ImageException:
-                    self.battle.mp_i.search_click(2)
+                    self.battle.mp_i.search_click_clear(2)
                 try:
                     self.battle.ability(1, -230, "blizzard_flask")
                     # self.battle.ability(1, -230, "fire_flask")
                 except ImageException:
-                    self.battle.mp_i.search_click(2)
+                    self.battle.mp_i.search_click_clear(2)
                 try:
                     self.battle.ability(2, -300, "blizzard_flask")
                     # self.battle.ability(2, -300, "fire_flask")
                 except ImageException:
-                    self.battle.mp_i.search_click(2)
+                    self.battle.mp_i.search_click_clear(2)
                 try:
                     self.battle.ability(3, -200, "meteor")
                     # self.battle.dual_ability(3, -400, "dual_black_magic", 210, "meteor")
                     # self.battle.ability(3, -400, "aero_blitz")
                 except ImageException:
-                    self.battle.mp_i.search_click(2)
+                    self.battle.mp_i.search_click_clear(2)
                 try:
                     self.battle.ability(4, -200, "meteor")
                     # self.battle.dual_ability(4, -400, "dual_black_magic", 210, "meteor")
                     # self.battle.ability(4, -400, "aero_blitz")
                 except ImageException:
-                    self.battle.mp_i.search_click(2)
+                    self.battle.mp_i.search_click_clear(2)
                 # exit()
                 self.battle.engage_all()
                 for _ in range(100):
